5d12b4ca3207d4b895351e37a9723a4605e59d01
franco25 <jhfranco.25@gmail.com>
2022-02-13 18:51:40 -0500

    social media meta

---
+++ semana3/dia4/diauno.js
@@ -0,0 +1,27 @@
+
+
+
+
+/*const primervalor = prompt("ingrese un valor")
+const segundovalor = prompt("ingrese un valor")
+
+let resultado = 0
+if (primervalor > segundovalor) {
+    console.log("el numero mayor es ", primervalor)
+} else if (primervalor < segundovalor) {
+    console.log("el numero mayor es ", segundovalor)
+}*/
+
+
+const numeroOne = +prompt("Ingrese un numero1");
+const numeroTwo = +prompt("Ingrese un numero2");
+const numerothree = +prompt("Ingrese un numero2");
+
+let mensajeRes = "Numero 2 es mayor";
+
+if (numeroOne > numeroTwo) {
+  mensajeRes = "Numero 1 es mayor";
+}
+
+console.log(mensajeRes);
+
